0acd09abbd8f95315f3488bb1217526f66b96eca
radityaputraa-sketch <radityathufail@gmail.com>
2026-04-26 09:49:00 +0700

    Include reportRoutes for report API functionality
    Added reportRoutes to handle report-related API endpoints.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import loanRoutes from './routes/loanRoutes.js';
 import memberRoutes from './routes/memberRoutes.js'; 
 import authorRoutes from './routes/authorRoutes.js';
 import categoryRoutes from './routes/categoryRoutes.js';
+import reportRoutes from './routes/reportRoutes.js';
 
 dotenv.config();
 
@@ -17,6 +18,7 @@ app.use('/api/loans', loanRoutes);
 app.use('/api/members', memberRoutes);
 app.use('/api/authors', authorRoutes);
 app.use('/api/categories', categoryRoutes);
+app.use('/api/reports', reportRoutes);
 
 app.get('/', (req, res) => res.send('Smart Library API is Running...'));
 
@@ -24,3 +26,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
+
+export default app;
